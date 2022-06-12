0ca92a3728e78e98d59855d8b4eecf6f63c3d540
Seth David Gyimah <esec26@gmail.com>
2022-06-12 17:28:49 +0000

    phase 1 array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, value) {
+    return drivers.filter(possibleMatch => possibleMatch.toLowerCase() === value.toLowerCase())
+}
+
+function fuzzyMatch(drivers, value) {
+    return drivers.filter(beginLetters => beginLetters.startsWith(value))
+}
+
+function matchName(drivers, value) {
+    return drivers.filter(stringValue => stringValue.name === value)
+}
